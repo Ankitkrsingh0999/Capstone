fd495e418b0e1d1a3bd052b972464bf5be8931fb
Devansh-Anhal <devanhal.da@gmail.com>
2019-03-21 15:22:55 +0530

    Update settings.py

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ed9rf=#9_*fn)o7^4jqcgqm&#(%4swa&itlw-q6la3!wjeqznl'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
